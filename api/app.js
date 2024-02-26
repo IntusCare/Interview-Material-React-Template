000df3df66750ae6882d8dad283a8ce7f283687e
davidrunion <dkrunion@gmail.com>
2024-02-25 23:03:34 -0500

    cleanup.. change cors policy to localhost-only (security), remove some create-react-app boilerplate

--- api/app.js
+++ api/app.js
@@ -4,7 +4,8 @@ const { participants } = require("./data");
 
 const app = express();
 
-app.use(cors());
+// Enable CORS for all routes to allow requests from https://localhost:3000 only
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get("/participants", (_, res) => {
   res.json(participants);
