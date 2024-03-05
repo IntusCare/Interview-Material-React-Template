c7fff0d800b9d02a38bd4ae3353d9cbf5718042e
Omkar Ubale <oubale@gmail.com>
2024-03-05 15:31:40 -0500

    CLEANUP: removed boiler plate assets

--- intus-care-fe-app/src/App.tsx
+++ intus-care-fe-app/src/App.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 import IntusCareNavbar from "./Navbar";
-import { Button } from "react-bootstrap";
 
 function App() {
   return (
